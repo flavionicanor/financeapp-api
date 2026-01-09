ee18c4609df7f710979f2fd1b3ad325e68eb8298
Flávio Nicanor <flaviorns@gmail.com>
2026-01-09 14:07:00 -0400

    chore: adicionado setup do Postgres

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import 'dotenv/config.js';
+import express from 'express';
+import { PostgresHelper } from './src/db/postgres/helper.js';
 
+const app = express();
+
+app.get('/', async (req, res) => {
+  const result = PostgresHelper.query('select * from users;');
+  res.send(JSON.stringify(result));
+});
+
+app.listen(3000, () => console.log('rodando na porta 3000'));
