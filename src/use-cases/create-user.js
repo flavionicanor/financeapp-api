39af2406d3f9a4f631136ebec9d80f1dc168d435
Flávio Nicanor <flaviorns@gmail.com>
2026-01-09 15:47:05 -0400

    feat: adicionado CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcryptjs';
+import { PostgresCreateUserRepository } from '../repositories/user-repository.js';
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    // verificar se o email ja esta cadastrado
+    // gerar ID do usuario
+    const userId = uuidv4();
+
+    // criptogrfar a senha
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    // gravar usuario no banco de dados
+    const newUSer = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = await postgresCreateUserRepository.execute(newUSer);
+    return createdUser;
+  }
+}
